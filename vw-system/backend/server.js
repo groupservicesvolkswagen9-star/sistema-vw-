3c62263bd9936ce899fe6ecc2e3ca94573e89d1c
Hernandez Zamora <DZM2QAL@vw.vwgroup.com>
2026-06-07 15:45:52 -0600

    fix puerto render

--- vw-system/backend/server.js
+++ vw-system/backend/server.js
@@ -39,6 +39,8 @@ app.post("/firmar", async (req,res)=>{
 });
 
 //////////////////////////////////////////////////////
-app.listen(3001, ()=>{
-  console.log("Servidor corriendo en http://localhost:3001");
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, ()=>{
+  console.log("Servidor corriendo en puerto " + PORT);
 });
