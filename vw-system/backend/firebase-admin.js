1481220020ef6baea8a41c2e7d6ed609c0fd30e2
Hernandez Zamora <DZM2QAL@vw.vwgroup.com>
2026-08-02 22:26:40 -0600

    debug firebase

--- vw-system/backend/firebase-admin.js
+++ vw-system/backend/firebase-admin.js
@@ -1,20 +1,18 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_SERVICE_ACCOUNT
+console.log(
+  "FIREBASE_SERVICE_ACCOUNT:",
+  !!process.env.FIREBASE_SERVICE_ACCOUNT
 );
 
-admin.initializeApp({
-  credential:
-    admin.credential.cert(
-      serviceAccount
-    )
-});
+const serviceAccount =
+  JSON.parse(
+    process.env.FIREBASE_SERVICE_ACCOUNT
+  );
 
-const db =
-  admin.firestore();
+console.log(
+  "PROJECT:",
+  serviceAccount.project_id
+);
 
-module.exports = {
-  admin,
-  db
-};
+process.exit(0);
